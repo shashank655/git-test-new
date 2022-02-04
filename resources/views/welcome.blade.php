05516729b126ec017b44783a99c6530b63b78226
shashank655 <shashankgarg655@gmail.com>
2022-02-04 15:56:55 +0530

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelassasas12
+                    Laravel
                 </div>
 
                 <div class="links">
